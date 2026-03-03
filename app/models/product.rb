e5e489c9064c70fdd24d4037049c3d61414678ed
Have Samuel <samiehave9@gmail.com>
2026-03-03 10:53:57 +0300

    Orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
-
   belongs_to :category
+  has_many :order_products
 end
